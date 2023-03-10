8640abb756e9697f7b4426c6ae539eb5ec37a762
hervouetdylan <dylan.hervouet@ynov.com>
2023-03-10 16:05:43 +0100

    test

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxhealth(maxHealth);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
