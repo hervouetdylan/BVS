47743b9dc8c52ee14879b334bfdefc86bbd5fd75
hervouetdylan <dylan.hervouet@ynov.com>
2023-03-15 14:28:01 +0100

    ajout de la bar de vie

---
+++ Assets/scripts/Character/PlayerHealth.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHeealth;
+    public HealthBar HealthBar;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
