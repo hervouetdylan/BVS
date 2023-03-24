37893491e237dc6bee7717d693f7b64f747efd98
hervouetdylan <dylan.hervouet@ynov.com>
2023-03-24 14:06:16 +0100

    barre de vie en place

--- Assets/scripts/Character/PlayerHealth.cs
+++ Assets/scripts/Character/PlayerHealth.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int currentHeealth;
-    public HealthBar HealthBar;
+    public int currentHealth;
+    public HealthBar healthBar;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
