012656e49030995016734a9768ddb828eb0dcf29
Marius BOURSE <marius.bourse@ynov.com>
2023-04-16 21:39:23 +0200

    stable version

--- Assets/scripts/UI/TitleScreenManager.cs
+++ Assets/scripts/UI/TitleScreenManager.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class TitleScreenManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public string NomDeScene;
 
+    public GameObject button;
+
+    public void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(button);
+
+    }
     public void AllezAuNiveau(string _sceneName)
     {
         SceneManager.LoadScene(_sceneName);
